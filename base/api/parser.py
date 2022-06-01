b0a8e72fb93ecd814d274d1cf034a4e827fa2907
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2022-06-01 11:39:07 +0900

    Seperated dynamic regex parser selection

--- base/api/parser.py
+++ base/api/parser.py
@@ -14,10 +14,25 @@ class RegexResponseParser(ResponseParser):
         try:
             return super().__getattribute__(name)
         except AttributeError:
-            if name.startswith('parse_one_'):
-                return (lambda *args, **kwargs: self.parse_one(name.split('parse_one_', 1)[-1].upper(), *args, **kwargs))
-            if name.startswith('parse_'):
-                return (lambda *args, **kwargs: self.parse(name.split('parse_', 1)[-1].upper(), *args, **kwargs))
+            self.dynamic_parse(name)
+    
+    def dynamic_parse(self, name):
+        if name.startswith('parse_one_'):
+            regex_name = name.split('parse_one_', 1)[-1].upper()
+            text_parser, response_parser = self.parse_one, self.parse_one_from_response
+        elif name.startswith('parse_'):
+            regex_name = name.split('parse_', 1)[-1].upper()
+            text_parser, response_parser = self.parse, self.parse_response
+        else:
+            raise AttributeError("Regex Response Parser has no attribute with name={}".format(name))
+        
+        if not hasattr(self.regex_collection, regex_name):
+            raise AttributeError("Regex with name={} is not found.".format(regex_name))
+        
+        return (lambda source, *args, **kwargs: 
+                    text_parser(regex_name, source, *args, **kwargs)
+                    if isinstance(source, str) else 
+                    response_parser(regex_name, source, *args, **kwargs))
     
     def __init__(self, regex_collection: type):
         self.regex_collection = regex_collection
