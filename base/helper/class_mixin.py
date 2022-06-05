66dc00eb44824a3e6b6309a227db2b55b7176e96
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2022-06-05 16:32:55 +0900

    Added LogGetattr Mixin

--- base/helper/class_mixin.py
+++ base/helper/class_mixin.py
@@ -1,4 +1,7 @@
 
+from types import NoneType
+from typing import Callable
+
 from ..data_structs import ReprCustomMapping
 
 
@@ -7,3 +10,12 @@ class ReprMixin:
     
     def __repr__(self):
         return self.__class__._repr_format % ReprCustomMapping.get_instance(self)
+
+
+class LogGetattrMixin:
+    LOGGER_FUNCTION: Callable[[str], NoneType] = print
+    def __getattribute__(self, name: str):
+        value = super().__getattribute__(name)
+        cls = super().__getattribute__('__class__')
+        cls.LOGGER_FUNCTION("%(classname)s.__getattribute__(%(repr)s,'%(name)s') -> %(value)s" % dict(classname=cls.__name__, repr=super().__getattribute__('__repr__')(), name=name, value=value))
+        return value
