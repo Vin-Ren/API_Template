78242e28ce7d9b4f9fb8b3d854d0178998f3ae79
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2022-06-10 17:18:29 +0900

    Abstracted BasePlugin methods

--- base/plugins/base.py
+++ base/plugins/base.py
@@ -2,12 +2,27 @@
 from ..api.api import API
 
 
-class BasePlugin:
-    def __getattribute__(self, name: str):
+class _BasePlugin:
+    ENABLE_GETITEM_VARIABLE_ACCESS = False
+    
+    def __getattribute__(self, name):
         try:
             return super().__getattribute__(name)
         except AttributeError:
             return self.api.__getattribute__(name)
     
+    def __getitem__(self, name):
+        if self.__class__.ENABLE_GETITEM_VARIABLE_ACCESS:
+            return self.__getattribute__(name)
+        else:
+            return super().__getitem__(name)
+    
     def __init__(self, api: API):
         self.api = api
+
+class BasePlugin(_BasePlugin):
+    def __getattribute__(self, name: str):
+        return super().__getattribute__(name)
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
