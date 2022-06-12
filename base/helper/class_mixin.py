775eacb643df5237ae663592802eb8760c5348cc
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2022-06-12 10:08:54 +0900

    Moved PluggableMixin to plugins.base

--- base/helper/class_mixin.py
+++ base/helper/class_mixin.py
@@ -1,9 +1,9 @@
 
 from types import NoneType
-from typing import Callable, Dict, List, Union
+from typing import Callable
 
-from ..data_structs import ObjectifiedDict, ReprCustomMapping
-from ..plugins.base import BasePlugin
+from ..data_structs import ReprCustomMapping
+from ..plugins.base import PluggableMixin
 
 
 class ReprMixin:
@@ -11,66 +11,6 @@ class ReprMixin:
     
     def __repr__(self):
         return self.__class__._repr_format % ReprCustomMapping.get_instance(self)
-
-
-class PluggableMixin:
-    PLUGINS: Union[List[BasePlugin], Dict[str, BasePlugin]] = []
-    PLUGINS_ACCESSABLE_THROUGH_INSTANCE_VARIABLE = False
-    ENABLE_DIRECT_GETATTR_PLUGINS_ACCESS = False
-    INHERIT_PLUGINS = True
-    
-    def __getattribute__(self, name: str):
-        try:
-            return super().__getattribute__(name)
-        except AttributeError as exc:
-            if not self.__class__.PLUGINS_ACCESSABLE_THROUGH_INSTANCE_VARIABLE:
-                pass
-
-            try:
-                return self.__getitem__(name)
-            except KeyError as exc:
-                pass
-
-            if not self.__class__.ENABLE_DIRECT_GETATTR_PLUGINS_ACCESS:
-                raise exc
-
-            if name in self.__pluggable_mixin_cached_method_table:
-                return self.__pluggable_mixin_cached_method_table[name]
-            
-            for plugin in self._plugins.values():
-                try:
-                    self.__pluggable_mixin_cached_method_table[name] = getattr(plugin, name)
-                    return self.__pluggable_mixin_cached_method_table[name]
-                except Exception as exc:
-                    pass
-            raise exc
-    
-    def __getitem__(self, name):
-        return self._plugins.__getitem__(name)
-    
-    def __init__(self, *args, **kwargs):
-        if isinstance(self.__class__.PLUGINS, dict):
-            # Updating plugins
-            if self.__class__.INHERIT_PLUGINS:
-                plugins = {}
-                [plugins.update(current_cls_plugin) for current_cls_plugin in reversed([cls.PLUGINS for cls in self.__class__.mro() if issubclass(cls, self.__class__) and isinstance(cls.PLUGINS, dict)])]
-                self.__class__.PLUGINS = plugins
-            
-            self._plugins = {name: plugin(self) for name, plugin in self.__class__.PLUGINS.items()}
-        else:
-            # Updating plugins
-            if self.__class__.INHERIT_PLUGINS:
-                plugins = []
-                [plugins.extend(current_cls_plugin) for current_cls_plugin in reversed([cls.PLUGINS for cls in self.__class__.mro() if issubclass(cls, self.__class__) and isinstance(cls.PLUGINS, list)])]
-                self.__class__.PLUGINS = list(set(plugins))
-            
-            if self.__class__.PLUGINS_ACCESSABLE_THROUGH_INSTANCE_VARIABLE:
-                self._plugins = {plugin.__name__: plugin(self) for plugin in self.__class__.PLUGINS}
-            else:
-                self._plugins = {plugin: plugin(self) for plugin in self.__class__.PLUGINS}
-        super().__init__(*args, **kwargs)
-        self._plugins = ObjectifiedDict(self._plugins)
-        self.__pluggable_mixin_cached_method_table = {}
 
 
 class LogGetattrMixin:
--- base/plugins/base.py
+++ base/plugins/base.py
@@ -1,4 +1,7 @@
 
+from typing import Union, List, Dict
+
+from ..data_structs import ObjectifiedDict
 from ..api.api import API
 
 
@@ -26,3 +29,61 @@ class BasePlugin(_BasePlugin):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+class PluggableMixin:
+    PLUGINS: Union[List[BasePlugin], Dict[str, BasePlugin]] = []
+    PLUGINS_ACCESSIBLE_THROUGH_INSTANCE_VARIABLE = False
+    ENABLE_DIRECT_GETATTR_PLUGINS_ACCESS = False
+    INHERIT_PLUGINS = True
+    
+    def __getattribute__(self, name):
+        try:
+            return super().__getattribute__(name)
+        except AttributeError as exc:
+            if self.__class__.PLUGINS_ACCESSIBLE_THROUGH_INSTANCE_VARIABLE:
+                try:
+                    return self.__getitem__(name)
+                except KeyError as exc:
+                    pass
+
+            if not self.__class__.ENABLE_DIRECT_GETATTR_PLUGINS_ACCESS:
+                raise exc
+
+            if name in self.__pluggable_mixin_cached_method_table:
+                return self.__pluggable_mixin_cached_method_table[name]
+            
+            for plugin in self._plugins.values():
+                try:
+                    self.__pluggable_mixin_cached_method_table[name] = getattr(plugin, name)
+                    return self.__pluggable_mixin_cached_method_table[name]
+                except Exception as exc:
+                    pass
+            raise exc
+    
+    def __getitem__(self, name):
+        return self._plugins.__getitem__(name)
+    
+    def __init__(self, *args, **kwargs):
+        if isinstance(self.__class__.PLUGINS, dict):
+            # Updating plugins
+            if self.__class__.INHERIT_PLUGINS:
+                plugins = {}
+                [plugins.update(current_cls_plugins) for current_cls_plugins in reversed([cls.PLUGINS for cls in self.__class__.mro() if issubclass(cls, self.__class__) and isinstance(cls.PLUGINS, dict)])]
+                self.__class__.PLUGINS = plugins
+            
+            self._plugins = {name: plugin(self) for name, plugin in self.__class__.PLUGINS.items()}
+        else:
+            # Updating plugins
+            if self.__class__.INHERIT_PLUGINS:
+                plugins = []
+                [plugins.extend(current_cls_plugins) for current_cls_plugins in reversed([cls.PLUGINS for cls in self.__class__.mro() if issubclass(cls, self.__class__) and isinstance(cls.PLUGINS, list)])]
+                self.__class__.PLUGINS = list(set(plugins))
+            
+            if self.__class__.PLUGINS_ACCESSIBLE_THROUGH_INSTANCE_VARIABLE:
+                self._plugins = {plugin.__name__: plugin(self) for plugin in self.__class__.PLUGINS}
+            else:
+                self._plugins = {plugin: plugin(self) for plugin in self.__class__.PLUGINS}
+        super().__init__(*args, **kwargs)
+        self._plugins = ObjectifiedDict(self._plugins)
+        self.__pluggable_mixin_cached_method_table = {}
